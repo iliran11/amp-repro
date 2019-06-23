78b655446231bfd0cf30321062884b938d6b13c3
iliran11 <lirancohen@Lirans-MacBook-Pro.local>
2019-06-24 02:37:02 +0300

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import { withAmp } from 'next/amp';
-import { Button } from 'liran/button';
-// import {Button} from '../button'
+// import { Button } from 'experiment/button';
+import {Button} from '../button'
 
 const Index = () => (
   <div>
     <p>Hello Next.js</p>
-    <Button>ff</Button>
+    <Button>hello</Button>
   </div>
 );
 
